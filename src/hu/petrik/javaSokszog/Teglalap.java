10d164c33829b782681144f57b4920e433ba113b
furedi daniel <daniel.furedi03@gmail.com>
2022-10-04 14:20:09 +0200

    teglalap metodusok

---
+++ src/hu/petrik/javaSokszog/Teglalap.java
@@ -0,0 +1,32 @@
+package hu.petrik.javaSokszog;
+
+public class Teglalap extends Sokszog{
+    private double b;
+
+    public Teglalap(double a, double b) {
+        super(a);
+        this.b = b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getKerulet(){
+        return 2 * (super.getA() + this.b);
+    }
+
+    public double getTerulet(){
+        return this.getA() * this.b;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Téglalap: a = %f; b = %f; K = %d, T = %f"
+                , this.getA(), this.getB(), this.getKerulet(), this.getTerulet()) ;
+    }
+}
